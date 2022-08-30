dc7bde43de65519eb0dbd0aaf9e80a5687bd8a49
jaeseo <jaeserrr@ewhain.net>
2022-08-30 17:53:45 +0900

    [맵과 셋] 선택 7785
    힌트 : 회사에 남아있는 사람만 알면 되겠어요!

---
+++ 09월 06일 - 맵과 셋/선택/7785.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main() {
+	int n;
+	string name, state;
+	set<string> company;
+
+	// 입력
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> name >> state;
+		company.insert(name);
+
+		if (state == "leave")
+			company.erase(name);
+	}
+
+	// 출력 (역으로 참조)
+	for (auto iter = company.rbegin(); iter != company.rend(); iter++) {
+		cout << *iter << '\n';
+	}
+}
