5155391037366d192e3e317042599c3612a29212
Dong-droid <80375422+Dong-droid@users.noreply.github.com>
2022-08-31 11:44:19 +0900

    Update 0906_1.cpp

--- 09월 06일 - 맵과 셋/필수/0906_1.cpp
+++ 09월 06일 - 맵과 셋/필수/0906_1.cpp
@@ -7,48 +7,34 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<int> v;
-int M;
-int reset_arr[21];
+const int SIZE=21;
 
-void Add(int n){
-    if(find(v.begin(),v.end(),n) == v.end()) v.push_back(n);
-}
-void Remove(int n){
-    auto iter = find(v.begin(),v.end(),n);
-    if(iter != v.end()) v.erase(iter); 
-}
-void print_check(int n){
-    if(find(v.begin(),v.end(),n) == v.end()) cout<<0<<'\n';
-    else cout<<1<<'\n';
-}
-void Toggle(int n){
-    auto iter = find(v.begin(),v.end(),n); 
-    if(iter == v.end()) v.push_back(n);
-    else v.erase(iter);
-}
-int main()
-{
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    string s;
-    int n; 
-    //입력
-    cin>>M;
-    // reset_arr를 초기화
-    for(int i = 1; i <= 20; i++) reset_arr[i-1] = i;
-    while(M--){
-        cin>>s;
-        if(s == "all") v.assign(reset_arr, reset_arr+20);
-        else if(s == "empty") v.clear();
-        else {
-             cin>>n;
-            if(s == "add") Add(n);
-            else if(s == "remove") Remove(n);
-            else if(s == "check") print_check(n); //출력
-            else if(s == "toggle") Toggle(n);
-        }
+int main(){
+ios::sync_with_stdio(0); cin.tie(0);
+//입력
+int n; cin>>n;
+vector<bool> s(SIZE,false); // 0~20의 유무를 나타낼 배열을 false로 초기화
+
+for(int i=0;i<n;i++){
+    string command;
+    cin>>command;
+
+    if(command=="all") 
+        s.assign(SIZE,true); //true의 값으로 SIZE개의 원소를 할당
+    else if(command=="empty") 
+        s.assign(SIZE,false);
+    else{
+        int x; cin>>x;
+        if(command=="add") 
+            s[x]=true;
+        else if(command=="remove") 
+            s[x]=false;
+        else if(command=="check") 
+            cout<<s[x]<<'\n'; // true이면 1을, false이면 0을 출력
+        else if(command=="toggle")
+            s[x]=!s[x]; // true이면 false를, false면 true를 대입
     }
-    return 0;
+}
 }
 /******************************************************************************
 
@@ -61,7 +47,7 @@ using namespace std;
 int M;
 int S, reset; // S의 20개의 비트가 1 부터 20까지 숫자의 유무(1 or 0)를 표현할 것입니다
 
-void Add(int n){
+void add(int n){
     if((S & (1 << n)) == 0) S |= (1 << n);
 }
 void Remove(int n){
@@ -71,16 +57,16 @@ void print_check(int n){
     if(S & (1 << n)) cout<<1<<'\n';
     else cout<<0<<'\n';
 }
-void Toggle(int n){
+void toggle(int n){
     if(S & (1 << n)) Remove(n);
-    else Add(n);
+    else add(n);
 }
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     //입력
     cin>>M;
-    //reset할 수를 초기화해줍니다
+    //명령이 all일 때 reset할 수를 초기화해줍니다
     for(int i = 1; i <= 20; i++) reset |= (1 << i);
     
     while(M--){
@@ -89,10 +75,10 @@ int main()
         else if(s == "empty") S = 0;
         else {
             int n; cin>>n;
-            if(s == "add") Add(n);
+            if(s == "add") add(n);
             else if(s == "remove") Remove(n);
             else if(s == "check") print_check(n); //출력
-            else if(s == "toggle") Toggle(n);
+            else if(s == "toggle") toggle(n);
         }
     }
     return 0;
