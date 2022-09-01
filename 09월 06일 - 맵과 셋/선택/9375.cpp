35bd440864e859cb67cb2bcec5211a917505da44
junghk0115 <89477748+junghk0115@users.noreply.github.com>
2022-09-02 00:16:25 +0900

    [맴과 셋] 선택 9375
    [힌트]
    의상의 이름과 의상의 종류 중 우리에게 필요한 입력값은 무엇일까요?
    마지막에 해빈이가 알몸이 되지 않도록 주의해주세요!

---
+++ 09월 06일 - 맵과 셋/선택/9375.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+/**
+ * 의상의 종류만 알면 된다.
+ */
+
+int main() {
+    int T, n;
+    string clothes, kind;
+
+    cin >> T;
+    while (T--) {
+        map<string, int> m;
+
+        //입력
+        cin >> n;
+        while (n--) {
+            cin >> clothes >> kind;
+            m[kind]++;
+        }
+
+        
+        int result = 1;
+        for (auto iter = m.begin(); iter != m.end(); iter++) {
+            result *= iter->second + 1; 
+        }
+
+        //출력
+        cout << result - 1 << '\n';  //알몸인 경우 제외
+    }
+}
