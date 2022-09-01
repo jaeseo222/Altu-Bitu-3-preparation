495d062373c10188c7eb0a812a49ffeb326f864b
dooli1971039 <dooli9819@gmail.com>
2022-09-01 17:28:14 +0900

    변수를 스네이크 표기법에 맞춰 작성함

--- 09월 06일 - 맵과 셋/필수/10757_v2.cpp
+++ 09월 06일 - 맵과 셋/필수/10757_v2.cpp
@@ -8,13 +8,13 @@ using namespace std;
     **이 문제에서는 굳이 참조자를 사용할 필요는 없지만, C++에서는 주로 &를 붙입니다**
 */
 stack<int> plusAB(string &a, string &b) {
-    int idxA = a.size() - 1; // 일의 자리 인덱스
-    int idxB = b.size() - 1;
+    int idx_a = a.size() - 1; // 일의 자리 인덱스
+    int idx_b = b.size() - 1;
     int carry = 0;
     stack<int> ans;
 
-    while (idxA >= 0 && idxB >= 0) {
-        int num = (a[idxA--] - '0') + (b[idxB--] - '0'); // 문자를 숫자로 바꾸어 더하기, 그리고 인덱스 변경
+    while (idx_a >= 0 && idx_b >= 0) {
+        int num = (a[idx_a--] - '0') + (b[idx_b--] - '0'); // 문자를 숫자로 바꾸어 더하기, 그리고 인덱스 변경
 
         num += carry;       // num에 carry 더해주기. (carry는 어차피 1 아니면 0)
         carry = num / 10;   // num의 값이 10이상이면 carry가 1, 아니면 0
@@ -22,8 +22,8 @@ stack<int> plusAB(string &a, string &b) {
     }
 
     // 1. a가 더 긴 경우
-    while (idxA >= 0) {
-        int num = a[idxA--] - '0';
+    while (idx_a >= 0) {
+        int num = a[idx_a--] - '0';
 
         num += carry;
         carry = num / 10;
@@ -31,8 +31,8 @@ stack<int> plusAB(string &a, string &b) {
     }
 
     // 2. b가 더 긴 경우
-    while (idxB >= 0) {
-        int num = b[idxB--] - '0';
+    while (idx_b >= 0) {
+        int num = b[idx_b--] - '0';
 
         num += carry;
         carry = num / 10;
