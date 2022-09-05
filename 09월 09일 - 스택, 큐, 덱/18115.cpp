a0b95db1505d1fc7788d5d6b5031b9951f85545a
junghk0115 <89477748+junghk0115@users.noreply.github.com>
2022-09-05 23:49:12 +0900

    [스택, 큐, 덱] 선택 18115
    [힌트]
    결과값을 보고 처음 상태를 유추하는 문제네요!
    결과에서부터 거슬러 올라가며 거꾸로 생각해볼까요?

---
+++ 09월 09일 - 스택, 큐, 덱/18115.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <deque>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+deque<int> cardGame(int n, vector<int>& cmd) {
+    deque<int> dq;
+
+    for (int i = 1; i <= n; i++) { //놓여 있는 위의 카드부터 검사 1, ..., n
+        switch (cmd[i]) {
+        case 1:
+            dq.push_front(i);
+            break;
+        case 2: {
+            int x = dq.front();
+            dq.pop_front();
+            dq.push_front(i);
+            dq.push_front(x);
+            break;
+        }
+        case 3:
+            dq.push_back(i);
+            break;
+        }
+    }
+}
+
+/**
+*결과값을 보고 처음 상태를 유출해야 한다.
+*기술을 모두 반대로 써보며, 결과값에서부터 거슬러 올라가자.
+*/
+
+int main() {
+    int n;
+
+    //입력
+    cin >> n;
+    vector<int> cmd(n + 1, 0);
+    for (int i = 1; i <= n; i++) { 
+        cin >> cmd[i];
+    }
+
+    //연산
+    reverse(cmd.begin() + 1, cmd.end()); //가장 위의 카드부터 검사하기 위해 기술 순서도 반대로
+    deque<int> dq = cardGame(n, cmd);
+
+    //출력
+    while (!dq.empty()) {
+        cout << dq.front() << ' ';
+        dq.pop_front();
+    }
+    return 0;
+}
