1efc790fce1149c62fe522dacabd41a600ba5b20
밍 <79096808+0321minji@users.noreply.github.com>
2022-09-14 09:39:57 +0900

    Update 3226.cpp

--- 09월 20일 - 구현 & 코너케이스/선택/3226.cpp
+++ 09월 20일 - 구현 & 코너케이스/선택/3226.cpp
@@ -5,19 +5,21 @@ using namespace std;
 int calculateCharge(int h, int m,int time,int charge) {
 	int end_h = h;
 	int end_m = (m + time) % 60;
-
+	//끝나는 시간 계산
 	if (m + time >= 60) {
 		end_h += 1;
 		if (end_h == 24) {
 			end_h = 0;
 		}
 	}
+	//요금이 중간에 바뀌는 경우
 	if (h == 6 && end_h == 7) {
 		charge += (time - end_m) * 5 + end_m * 10;
 	}
 	else if (h == 18 && end_h == 19) {
 		charge += (time - end_m) * 10 + end_m * 5;
 	}
+	//바뀌지 않는 경우
 	else {
 		if (6 < h && h < 19) {
 			charge += 10 * time;
