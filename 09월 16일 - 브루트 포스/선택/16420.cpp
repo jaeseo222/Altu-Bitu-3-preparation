3dc51bc688ba61590c4dfdf3bd71dbdef7100058
김서연 <86418674+flowersayo@users.noreply.github.com>
2022-09-19 10:29:21 +0900

    Update 16420.cpp

--- 09월 16일 - 브루트 포스/선택/16420.cpp
+++ 09월 16일 - 브루트 포스/선택/16420.cpp
@@ -1,96 +1,81 @@
+//
+// Created by BaeSuA on 2022-09-10.
+//
+
 #include <iostream>
 #include <vector>
-#define INF 987654321
+
 using namespace std;
+const int MAX = 3000;
 
-typedef pair<int, int> ci;
 
-int dx[5] = { 0,0,0,-1,1 };
-int dy[5] = { 0,1,-1,0,0 };
-vector<vector<bool>> board; 
-vector<vector<int>> cost;
-int min_cost= INF;
+vector<vector<int>> board;
+vector<vector<bool>> check;
+int dr[5] = { 0, -1, 1, 0, 0 };
+int dc[5] = { 0, 0, 0, -1, 1 };
+int ans = MAX;
 
-bool check(int x, int y) { //꽃을 심을 수 있는지 확인
-
-	
-	for (int i = 0; i < 5; i++) {
-	
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-
-		if (board[nx][ny]) //무언가 이미 심어져 있다면
-			return false;
-	}
-
-	return true;
-	
+int chkFlower(int r, int c) {
+  
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        if (check[nr][nc]) { // 이미 꽃 있다면
+            return -1;
+        }
+        sum += board[nr][nc];
+    }
+    return sum;
 }
-void plant(int x,int y,bool marking_type) { //상하좌우로 꽃 심거나 돌려놓기.
+void plant(int r, int c, bool marking_type) { //상하좌우로 꽃 심거나 돌려놓기.
 
 
-	for (int i = 0; i < 5; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
+    for (int i = 0; i < 5; i++) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
 
-		board[nx][ny] = marking_type;
-	}
+        check[nr][nc] = marking_type;
+    }
 
 }
+void minFlower(int cnt, int sum) {
 
-
-void dfs(int cnt) { //cnt : 꽃을 몇개 심었는지
-
-	if (cnt == 3) {
-		int total = 0;
-
-		// 꽃이 심어진 영역의 비용 계산하기
-		for (int i = 0; i < board.size() ; i++) {
-			for (int j = 0; j < board.size(); j++) {
-
-				if (board[i][j])
-
-					total += cost[i][j];
-			}
-
-		}
-
-		min_cost = min(min_cost, total);
-		return;
-	}
-
-	// 꽃 심을 위치 정하기
-	for (int i = 1; i < board.size() - 1; i++) {
-		for (int j = 1; j < board.size() - 1; j++) {
-
-			if (check(i, j)) { // 꽃을 심을 수 있다면
-				plant(i, j, 1); 
-				dfs(cnt + 1); 
-				plant(i, j, 0); // 원상복구
-
-			}
-		}
-	}
-
+    if (cnt == 3) {
+        ans = min(ans, sum);
+        return;
+    }
+    for (int i = 1; i < board.size()-1; i++) {
+        for (int j = 1; j < board.size() - 1; j++) {
+            int sumFlower = chkFlower(i, j);
+            if (sumFlower == -1) {
+                continue;
+            }
+            plant(i, j, 1); 
+            minFlower(cnt + 1, sum + sumFlower);
+            plant(i, j, 0);
+        }
+    }
 }
+
 int main() {
-	
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
 
-	int n;
-	cin >> n;
+    int n; 
 
-	cost.assign(n, vector<int>(n, 0));
-	board.assign(n, vector<bool>(n, false));
+    // 입력
+    cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> cost[i][j];
-		}
-	}
+    board.assign(n, vector<int>(n, 0));
+    check.assign(n, vector<bool>(n, false));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
+        }
+    }
 
-	dfs(0);
-	cout << min_cost;
+    // 연산 & 출력
+    minFlower(0, 0);
 
+    cout << ans;
+    return 0;
 }
