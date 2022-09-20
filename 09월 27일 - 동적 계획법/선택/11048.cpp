2f3f80dfc9a3e705890703dfe55cfd4b5efa997a
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-09-20 17:05:00 +0900

    [동적계획법] 선택 11048

---
+++ 09월 27일 - 동적 계획법/선택/11048.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+* 동적프로그래밍을 이용하여 값을 업데이트
+* 첫 행과 열은 따로 처리
+*/
+
+int main()
+{
+	// 입력
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> dp(n, vector<int>(m));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> dp[i][j];
+		}
+	}
+	// 연산
+	// 첫 번째 세로 열
+	for (int i = 1; i < n; i++) {
+		dp[i][0] += dp[i - 1][0];
+	}
+	// 첫 번째 가로 행
+	for (int i = 1; i < m; i++) {
+		dp[0][i] += dp[0][i-1];
+	}
+	// 나머지 부분
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j < m; j++) {
+			dp[i][j] += max({ dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1] }); // 가장 큰 값으로 업데이트
+		}
+	}
+	// 출력
+	cout << dp[n - 1][m - 1]; 
+	return 0;
+}
