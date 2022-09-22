c96f73ed6ed0192bf19244e569098b97ee3ec1a9
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-09-22 14:01:41 +0900

    [동적계획법] 선택 11048

--- 09월 27일 - 동적 계획법/선택/11048.cpp
+++ 09월 27일 - 동적 계획법/선택/11048.cpp
@@ -6,26 +6,18 @@ using namespace std;
 
 /*
 * 동적프로그래밍을 이용하여 값을 업데이트
-* 첫 행과 열은 따로 처리
+* 더미 인덱스를 사용하여 예외 없이 한 번에 처리
 */
 
 int maxCandy(int n, int m, vector<vector<int>>dp)
 {
-	// 첫 번째 열
-	for (int i = 1; i < n; i++) {
-		dp[i][0] += dp[i - 1][0];
-	}
-	// 첫 번째 행
-	for (int i = 1; i < m; i++) {
-		dp[0][i] += dp[0][i - 1];
-	}
-	// 나머지 부분
-	for (int i = 1; i < n; i++) {
-		for (int j = 1; j < m; j++) {
+	// 동적 계획법으로 사탕의 개수 계산
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
 			dp[i][j] += max({ dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1] }); // 가장 큰 값으로 업데이트
 		}
 	}
-	return dp[n - 1][m - 1];
+	return dp[n][m];
 }
 
 int main()
@@ -33,13 +25,13 @@ int main()
 	// 입력
 	int n, m;
 	cin >> n >> m;
-	vector<vector<int>> maze(n, vector<int>(m));
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+	vector<vector<int>> maze(n+1, vector<int>(m+1));
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
 			cin >> maze[i][j];
 		}
 	}
 	// 연산 & 출력
-	cout << maxCandy(n, m, maze); 
+	cout << maxCandy(n, m, maze);
 	return 0;
 }
