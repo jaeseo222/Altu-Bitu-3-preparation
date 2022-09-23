47abc5c439d56eb931561c420cd3953ca7260af6
밍 <79096808+0321minji@users.noreply.github.com>
2022-09-23 14:40:11 +0900

    Update 2156.cpp

--- 09월 27일 - 동적 계획법/선택/2156.cpp
+++ 09월 27일 - 동적 계획법/선택/2156.cpp
@@ -7,21 +7,12 @@ using namespace std;
 int maxWine(int n, vector<int> wine) {
 	vector<int> dp(n, 0);
 	dp[0] = wine[0];
-	
-	/*
-	입력이 3 이하일 경우는 for문 안에 넣어서 check
-	max() 함수 사용 시 3개 이상의 값 비교시 {}로 묶으면 한 번에 사용 가능합니다!
-	*/
-	
-	for (int i = 1; i < n; i++) {
-		if (i < 2) {
-			dp[1] = wine[0] + wine[1];
-			continue;
-		}
-		if (i < 3) {
-			dp[2] = max({ wine[1] + wine[2], wine[0] + wine[2], dp[1] });
-			continue;
-		}
+	dp[1] = wine[0] + wine[1];
+	dp[2] = max({ wine[1] + wine[2], wine[0] + wine[2], dp[1] });
+
+	//max() 함수 사용 시 3개 이상의 값 비교시 {}로 묶으면 한 번에 사용 가능합니다!
+	for (int i = 3; i < n; i++) {
+		//가장 큰 값으로 갱신
 		dp[i] = max({ dp[i - 3] + wine[i - 1] + wine[i],dp[i - 2] + wine[i],dp[i - 1] });
 	}
 	return dp[n - 1];
