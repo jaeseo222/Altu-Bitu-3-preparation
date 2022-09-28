8c799d041b83d191271fa6fdb67af6f2aa2809a8
dbswn <87461479+dbswn@users.noreply.github.com>
2022-09-29 01:29:15 +0900

    Update 23757.cpp

--- 09월 30일 - 우선순위 큐/선택/23757.cpp
+++ 09월 30일 - 우선순위 큐/선택/23757.cpp
@@ -10,22 +10,13 @@ using namespace std;
 * Hint 아이들이 선택한 상자는 어떤 특징이 있나요?
 */
 
-
-int solution(int n, int m) {
-
-	
-	return 1;
-}
-
-
 int main() {
-	int n, m;
-	int flag = 1;
-	cin >> n >> m;
-
-	int tmp, child_want, max;
+	int n, m, tmp, child_want, max;
 	priority_queue<int> max_heap;
-
+	int flag = 1;
+	
+	cin >> n >> m;
+	
 	//우선순위큐에 선물 삽입
 	for (int i = 0; i < n; i++) {
 		cin >> tmp;
