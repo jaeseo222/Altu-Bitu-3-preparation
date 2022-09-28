9cd5993b04c69d6d18b39a96be2b2211521edb83
밍 <79096808+0321minji@users.noreply.github.com>
2022-09-28 19:29:20 +0900

    [그리디] 선택 10610
    [힌트]
    문제 풀이에 대한 힌트를 제공할 지 입출력에 대해 힌트를 제공할 지 고민입니다ㅜㅜ
    문제 풀이에 대해서는 오히려 빨리 생각할 수 있을 것 같은데 실제로 제출 시에 많이 놓치는 부분이 입출력인 것 같아서요...!
    
    -> 30의 배수인지 어떻게 확인할 수 있을까요?
    -> 입력의 크기가 어떻게 되는지 확인해보세요!
    
    의견 주시면 감사하겠습니다!

---
+++ 10월 04일 - 그리디 알고리즘/선택/10610.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+
+/*
+[가장 큰 수]
+-입력 받은 각 수를 가장 큰 순서대로 정렬
+
+[30의 배수 조건]
+-3의 배수 : 각 자리의 수의 합이 3의 배수
+-10의 배수 : 일의 자리가 0
+*/
+
+//모든 자릿수 합이 3의 배수인지 확인하는 함수
+bool checkThree(string num) {
+    int sum = 0;
+    for (int i = 0; i < num.size(); i++) {
+        sum += num[i];
+    }
+
+    //더한 자릿수의 합이 3의 배수이면 num도 3의 배수
+    if (sum % 3 == 0) {
+        return true;
+    }
+    return false;
+}
+
+//n으로 만들 수 있는 30의 배수 중 가장 큰 수를 구하는 함수
+string findNum(string n) {
+    sort(n.begin(), n.end(), greater<>()); //각 자릿수 내림차순 정렬
+
+    //30의 배수이면 숫자 리턴
+    if (n[n.size() - 1] == '0' && checkThree(n)) {
+        return n;
+    }
+    return "-1";
+
+}
+
+int main() {
+    string n;
+    cin >> n;
+
+    string ans = findNum(n);
+
+    //N이 최대 10^5개의 숫자로 구성되어 있으므로 string타입으로 하나씩 출력
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i];
+    }
+    cout << '\n';
+    return 0;
+
+}
