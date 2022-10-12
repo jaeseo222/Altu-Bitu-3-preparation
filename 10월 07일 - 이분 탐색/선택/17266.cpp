e861972aa9848532bb9dae13b100193c327615ea
HyegyoJung <junghk0115@gmail.com>
2022-10-12 16:40:13 +0900

    [이분탐색] 선택 17266 update

--- 10월 07일 - 이분 탐색/선택/17266.cpp
+++ 10월 07일 - 이분 탐색/선택/17266.cpp
@@ -3,37 +3,38 @@
 
 using namespace std;
 
-//임의로 설정한 가로등의 높이와 가로등 사이의 거리를 비교해서 가로등 사이 밝히지 못한 구간이 있는지 확인.
-int solution(int n, int m, vector<int> lights) {
-    int start = 0;
-    int end = n;
+//모든 구간을 밝힐 수 있는지 확인하는 함수
+bool check(int n, int m, vector<int> lamp, int mid) {
 
-    while (start <= end) {
-        int mid = (start + end) / 2;
-        bool check = true; //모든 구간을 밝힐 수 있는가
-
-        if (lights[0] > mid) { //시작 구간 확인
-            check = false;
+    for (int i = 0; i < m; i++) { //가로등 사이 구간 확인
+        if (lamp[0] > mid) { //시작 구간 확인
+            return false;
         }
 
-        for (int i = 1; i < m; i++) { //가로등 사이 구간 확인
-            if (lights[i] - lights[i - 1] > mid * 2) {
-                check = false;
-                break;
-            }
-        }
-
-        if (n - lights[m - 1] > mid) { //끝 구간 확인
-            check = false;
-        }
-
-        if (!check) {
-            start = mid + 1;
-        } else {
-            end = mid - 1;
+        if (lamp[i] - lamp[i - 1] > mid * 2) {
+            return false;
         }
     }
-    return start;
+
+    if (n - lamp[m - 1] > mid) { //끝 구간 확인
+        return false;
+    }
+    return true;
+}
+
+//임의로 설정한 가로등의 높이와 가로등 사이의 거리를 비교해서 가로등 사이 밝히지 못한 구간이 있는지 확인.
+int parametricSearch(int n, int m, vector<int> lamp, int left, int right) {
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (!check(n, m, lamp, mid)) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return right + 1;
 }
 
 int main() {
@@ -41,13 +42,13 @@ int main() {
 
     //입력
     cin >> n >> m;
-    vector<int> lights(m, 0);
+    vector<int> lamp(m, 0);
     for (int i = 0; i < m; i++) {
-        cin >> lights[i];
+        cin >> lamp[i];
     }
 
     //연산, 출력
-    cout << solution(n, m, lights);
+    cout << parametricSearch(n, m, lamp, 0, n);
 
     return 0;
 }
