bdcb7ef8720760cde71a8b68b3f8b780f1f93564
HyegyoJung <junghk0115@gmail.com>
2022-10-30 05:34:43 +0900

    Update 6159.cpp

--- 10월 11일 - 투 포인터/선택/6159.cpp
+++ 10월 11일 - 투 포인터/선택/6159.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+/**
+ * 서로 다른 2개씩 짝 짓는 경우이므로 양 끝 포인터에서 시작 (오름차순 정렬을 미리 해줌)
+ * 무게 합이 S 초과라면 더 작은 쪽으로 이동하기 위해 right 포인터 감소
+ * 무게 합이 S 이하라면 left ~ right 사이에 있는 값과 현재 left와 짝 지었을 때도 S보다 작다는 것이므로 카운트 추가
+ */
+
 int solution(int n, int s, vector<int> cow) {
     int cnt = 0;
     int left = 0;
