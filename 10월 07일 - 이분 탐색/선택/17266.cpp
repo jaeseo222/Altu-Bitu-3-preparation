f3edf63ff9eb7d9a04c1fe9011dc4d59a5c3e23f
HyegyoJung <junghk0115@gmail.com>
2022-10-30 06:26:36 +0900

    Update 17266.cpp

--- 10월 07일 - 이분 탐색/선택/17266.cpp
+++ 10월 07일 - 이분 탐색/선택/17266.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 //모든 구간을 밝힐 수 있는지 확인하는 함수
-bool check(int n, int m, vector<int> lamp, int mid) {
+bool check(int n, int m, vector<int> &lamp, int mid) {
 
-    for (int i = 0; i < m; i++) { //가로등 사이 구간 확인
-        if (lamp[0] > mid) { //시작 구간 확인
-            return false;
-        }
+    if (lamp[0] > mid) { //시작 구간 확인
+        return false;
+    }
 
+    for (int i = 1; i < m; i++) { //가로등 사이 구간 확인
         if (lamp[i] - lamp[i - 1] > mid * 2) {
             return false;
         }
@@ -23,7 +23,7 @@ bool check(int n, int m, vector<int> lamp, int mid) {
 }
 
 //임의로 설정한 가로등의 높이와 가로등 사이의 거리를 비교해서 가로등 사이 밝히지 못한 구간이 있는지 확인.
-int parametricSearch(int n, int m, vector<int> lamp, int left, int right) {
+int parametricSearch(int n, int m, vector<int> &lamp, int left, int right) {
 
     while (left <= right) {
         int mid = (left + right) / 2;
