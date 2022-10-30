1481b83e1e6a919db81e2cdb950c481d2103b699
kwakrhkr59@gmail.com <112487814+qkqkqk7125@users.noreply.github.com>
2022-10-30 16:10:17 +0900

    Update 7662.cpp

--- 10월 11일 - 투 포인터/필수/7662.cpp
+++ 10월 11일 - 투 포인터/필수/7662.cpp
@@ -2,13 +2,12 @@
 #include <queue>
 #include <map>
 using namespace std;
-typedef long long ll;   //int 사용시 입력값으로 INT_MIN 들어올 경우, - (마이너스) 연산 시 INT 범위를 벗어나므로 에러 발생
 
 /*
  * [힌트] 최솟값이라고 최소힙을 쓰라는 보장은 없죠.
  *        대신 최댓값과 최솟값을 따로 관리해주려면 해당 원소가 몇 개가 남았는지도 알아둬야겠어요!
  */
-void popDummies(priority_queue<ll> &heap, map<ll, ll> &remained, int x) {
+void popDummies(priority_queue<int> &heap, map<int, int> &remained, int x) {
     while(!heap.empty() && remained[heap.top() * x]==0) {
         heap.pop();
     }
@@ -16,34 +15,39 @@ void popDummies(priority_queue<ll> &heap, map<ll, ll> &remained, int x) {
 int main() {
     int t; cin>>t;  //테스트 케이스
     while(t--) {
-        priority_queue<ll> max_queue, min_queue; //max_queue: 최대값 pop, min_queue: 최솟값 pop
-        map<ll, ll> remained; //큐에 남아있는 원소의 개수 저장
+        priority_queue<int> max_queue; //max_queue: 최대값 pop, min_queue: 최솟값 pop
+        priority_queue<int, vector<int>, greater<int>> min_queue;
+        map<int, int> remained; //큐에 남아있는 원소의 개수 저장
 
         int k; cin>>k;  //입력 개수
         while(k--) {
             char op; //연산자
-            ll x;    //피연산자
+            int x;    //피연산자
             cin>>op>>x;
 
             switch(op) {
                 case 'I':
                     max_queue.push(x);
-                    min_queue.push(-x); //최솟값 찾을 수 있도록 부호를 바꾸어 저장
+                    min_queue.push(x); //최솟값 찾을 수 있도록 부호를 바꾸어 저장
                     remained[x]++;  //원소 개수 1 증가
                     break;
 
                 case 'D':
                     if(x==-1) {
                         //이미 삭제된 값은 pop 하기
-                        popDummies(min_queue, remained, x);
+                        while(!min_queue.empty() && remained[min_queue.top()]==0) {
+                            min_queue.pop();
+                        }
                         //최솟값 pop
                         if(!min_queue.empty()) {
-                            remained[-min_queue.top()]--;
+                            remained[min_queue.top()]--;
                             min_queue.pop();
                         }
                     } else {
                         //이미 삭제된 값은 pop 하기
-                        popDummies(max_queue, remained, x);
+                        while(!max_queue.empty() && remained[max_queue.top()]==0) {
+                            max_queue.pop();
+                        }
                         //최댓값 pop
                         if(!max_queue.empty()) {
                             remained[max_queue.top()]--;
@@ -53,13 +57,17 @@ int main() {
             }
         }
         //이미 삭제된 값은 pop 하기
-        popDummies(min_queue, remained, -1);
-        popDummies(max_queue, remained, 1);
+        while(!min_queue.empty() && remained[min_queue.top()]==0) {
+            min_queue.pop();
+        }
+        while(!max_queue.empty() && remained[max_queue.top()]==0) {
+            max_queue.pop();
+        }
         
         if(max_queue.empty()) {
             cout<<"EMPTY\n";
         } else {
-            cout<<max_queue.top()<<" "<<-min_queue.top()<<"\n";
+            cout<<max_queue.top()<<" "<<min_queue.top()<<"\n";
         }
     }
 
