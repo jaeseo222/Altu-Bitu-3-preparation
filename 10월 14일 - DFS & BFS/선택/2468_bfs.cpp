578e142e59d8032b407fb7ecff5e4cfd1493e37f
Minseo10 <tahitiro2@gmail.com>
2022-10-31 13:03:53 +0900

    update 2468_bfs.cpp 2468_dfs.cpp

--- 10월 14일 - DFS & BFS/선택/2468_bfs.cpp
+++ 10월 14일 - DFS & BFS/선택/2468_bfs.cpp
@@ -1,20 +1,16 @@
 #include<iostream>
 #include<queue>
 #include<vector>
-#include<memory.h>
+#include<algorithm>
 using namespace std;
 
-const int MAX = 100;
-int board[MAX][MAX]={0,}; //지역들의 높이 정보 저장
-int visit[MAX][MAX]={false,}; //방문 여부 저장
-
 //높이가 heigt이하인 지점이 물에 잠길때의 bfs함수: queue 이용
-void bfs(int x, int y, int height, int n) {
+void bfs(int r, int c, int height, int n, vector<vector<int>> board, vector<vector<bool>>& visit) {
     queue<pair<int,int>> q;
-    int dx[4]={-1, 0, 1, 0};
-    int dy[4]={0,-1,0,1};
-    visit[x][y] = true;
-    q.push(make_pair(x,y));
+    int dr[4]={-1, 0, 1, 0};
+    int dc[4]={0, -1, 0, 1};
+    visit[r][c] = true;
+    q.push(make_pair(r,c));
 
     while(!q.empty()) {
         int a = q.front().first;
@@ -22,42 +18,33 @@ void bfs(int x, int y, int height, int n) {
         q.pop();
 
         for(int i=0; i<4; i++) {
-            int nx = a + dx[i]; //상하좌우로 이동 후의 좌표
-            int ny = b + dy[i];
+            int nr = a + dr[i]; //상하좌우로 이동 후의 좌표
+            int nc = b + dc[i];
 
-            if(nx < 0 || ny < 0 || nx >= n || ny >= n) { //board영역 밖에 있을때
+            if(nr < 0 || nc < 0 || nr >= n || nc >= n) { //board영역 밖에 있을때
                 continue;
             }
 
-            if(!visit[nx][ny] && board[nx][ny] > height) { //방문 안했고+물에 안 잠겼으면 방문해준다
-                visit[nx][ny] = true;
-                q.push(make_pair(nx,ny));
+            if(!visit[nr][nc] && board[nr][nc] > height) { //방문 안했고+물에 안 잠겼으면 방문해준다
+                visit[nr][nc] = true;
+                q.push(make_pair(nr,nc));
             }
         }
     }
 }
 
-int main() {
-    //입력
-    int n;
-    cin >>n;
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin >> board[i][j];
-        }
-    }
-
-    //연산
+//물에 잠기지 않는 영역의 최대 개수를 계산하는 함수
+int countArea(int n, vector<vector<int>> board, int max_height) { 
     int max_cnt = 1; //물에 잠기지 않는 영역의 최대 개수
-    for(int height=1; height<=MAX; height++) { //잠기는 높이를 변화시킨다
-        memset(visit, 0, sizeof(visit)); //visit배열 초기화
+
+    for (int height = 1; height <= max_height - 1; height++) { //잠기는 높이를 변화시킨다
+        vector<vector<bool>> visit(n, vector<bool>(n, false)); //방문 여부 저장
 
         int cnt = 0; //물에 잠기지 않는 영역의 개수
-        for (int i=0; i<n; i++) { //각 지역을 돌며 방문, 영역의 개수 카운트
-            for (int j=0; j<n; j++) {
+        for (int i = 0; i < n; i++) { //각 지역을 돌며 방문, 영역의 개수 카운트
+            for (int j = 0; j < n; j++) {
                 if (!visit[i][j] && board[i][j] > height) {
-                    bfs(i, j, height, n);
+                    bfs(i, j, height, n, board, visit); //bfs
                     cnt++;
                 }
             }
@@ -65,7 +52,24 @@ int main() {
         max_cnt = max(max_cnt, cnt);
     }
 
-    //출력
-    cout<<max_cnt<<'\n';
+    return max_cnt;
+}
+
+int main() {
+    //입력
+    int n;
+    cin >>n;
+
+    vector<vector<int>> board(n, vector<int>(n, 0)); //지역들의 높이 정보 저장
+    int max_height = 0; //높이의 최대값
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> board[i][j];
+            max_height = max(board[i][j], max_height);
+        }
+    }
+
+    //연산, 출력
+    cout<<countArea(n, board, max_height) << '\n';
     return 0;
 }
--- 10월 14일 - DFS & BFS/선택/2468_dfs.cpp
+++ 10월 14일 - DFS & BFS/선택/2468_dfs.cpp
@@ -1,53 +1,40 @@
 #include<iostream>
-#include<memory.h>
+#include<vector>
 #include<algorithm>
 using namespace std;
 
-const int MAX = 100;
-int board[MAX][MAX]={0,}; //지역들의 높이 정보 저장
-bool visit[MAX][MAX]={false,}; //방문 여부 저장
-
 //높이가 heigt이하인 지점이 물에 잠길때의 dfs함수
-void dfs(int x, int y, int height, int n){
-    int dx[4]={-1, 0, 1, 0};
-    int dy[4]={0,-1,0,1};
+void dfs(int r, int c, int height, int n, vector<vector<int>>& board, vector<vector<bool>>& visit){
+    int dr[4]={-1, 0, 1, 0};
+    int dc[4]={0, -1, 0, 1};
+    visit[r][c] = true;
 
-    visit[x][y] = true;
     for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i]; //상하좌우로 이동 후의 좌표
-        int ny = y + dy[i];
+        int nr = r + dr[i]; //상하좌우로 이동 후의 좌표
+        int nc = c + dc[i];
 
-        if (nx < 0 || ny < 0 || nx >= n || ny >= n) { //board영역 밖에 있을때
+        if (nr < 0 || nc < 0 || nr >= n || nc >= n) { //board영역 밖에 있을때
             continue;
         }
 
-        if(!visit[nx][ny] && board[nx][ny]>height){ //방문 안했고+물에 안 잠겼으면 방문해준다
-            dfs(nx, ny, height, n);
+        if(!visit[nr][nc] && board[nr][nc]>height){ //방문 안했고+물에 안 잠겼으면 방문해준다
+            dfs(nr, nc, height, n, board, visit);
         }
     }
 }
 
-int main(){
-    //입력
-    int n;
-    cin >>n;
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin >> board[i][j];
-        }
-    }
-
-    //연산
+//물에 잠기지 않는 영역의 최대 개수를 계산하는 함수
+int countArea(int n, vector<vector<int>> board, int max_height) { 
     int max_cnt = 1; //물에 잠기지 않는 영역의 최대 개수
-    for(int height=1; height<=MAX; height++){ //잠기는 높이를 변화시킨다
-        memset(visit,false,sizeof(visit)); //visit배열 초기화
 
-        int cnt=0; //물에 잠기지 않는 영역의 개수
-        for(int i=0;i<n;i++){ //각 지역을 돌며 방문,영역의 개수 카운트
-            for(int j=0;j<n;j++){
-                if(!visit[i][j] && board[i][j]>height){
-                    dfs(i,j,height,n);
+    for (int height = 1; height <= max_height - 1; height++) { //잠기는 높이를 변화시킨다
+        vector<vector<bool>> visit(n, vector<bool>(n, false)); //방문 여부 저장
+
+        int cnt = 0; //물에 잠기지 않는 영역의 개수
+        for (int i = 0; i < n; i++) { //각 지역을 돌며 방문,영역의 개수 카운트
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j] && board[i][j] > height) {
+                    dfs(i, j, height, n, board, visit); //dfs
                     cnt++;
                 }
             }
@@ -55,7 +42,24 @@ int main(){
         max_cnt = max(max_cnt, cnt);
     }
 
-    //출력
-    cout<<max_cnt<<'\n';
+    return max_cnt;
+}
+
+int main(){
+    //입력
+    int n;
+    cin >>n;
+
+    vector<vector<int>> board(n, vector<int>(n, 0)); //지역들의 높이 정보 저장
+    int max_height = 0; //높이의 최대값
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> board[i][j];
+            max_height = max(board[i][j], max_height);
+        }
+    }
+
+    //연산, 출력
+    cout<<countArea(n, board, max_height) << '\n';
     return 0;
 }
