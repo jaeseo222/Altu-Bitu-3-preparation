4298b03c2bb2c062ddccc9797a86739af23996d1
grdnr13 <77968137+grdnr13@users.noreply.github.com>
2022-10-31 10:22:47 +0900

    Update 1244.cpp

--- 10월 11일 - 투 포인터/필수/1244.cpp
+++ 10월 11일 - 투 포인터/필수/1244.cpp
@@ -2,9 +2,7 @@
 #include<vector>
 using namespace std;
 
-typedef vector<int> vi;
-
-void men(int num, vi &sw) {
+void men(int num, vector<int> &sw) {
 	int idx = num-1;
 	while (idx < sw.size()) { //자신이 받은 숫자의 배수 인덱스 스위치 상태 변경
 		sw[idx] = 1 - sw[idx];
@@ -12,7 +10,7 @@ void men(int num, vi &sw) {
 	}
 }
 
-void women(int num, vi& sw) {
+void women(int num, vector<int>& sw) {
 	//자신의 스위치 기준, 좌우가 대칭이면서 가장 많은 스위치를 포함하는 구간의 스위치 상태 변경
 	int i = 0;
 	while (num - i >= 0 && num + i < sw.size()) {
@@ -26,21 +24,10 @@ void women(int num, vi& sw) {
 	}
 }
 
-void printRes(vi &sw) {
-	//결과 출력
-	for (int i = 0; i < sw.size(); i++) {
-		cout << sw[i] << ' ';
-		if (i % 20 == 19) {
-			cout << '\n';
-		}
-	}
-}
-
-
 int main() {
 	int students, n;
 	cin >> n;
-	vi sw(n); //스위치 상태 저장할 벡터
+	vector<int> sw(n); //스위치 상태 저장할 벡터
 	for (int i = 0; i < n; i++) {
 		cin >> sw[i]; //초기 스위치 상태
 	}
@@ -56,6 +43,12 @@ int main() {
 			women(num - 1, sw);
 		}
 	}
-	printRes(sw);
+	
+	for (int i = 0; i < sw.size(); i++) {
+		cout << sw[i] << ' ';
+		if (i % 20 == 19) {
+			cout << '\n';
+		}
+	}
 
 }
