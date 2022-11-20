7a13c0d4f784d7dbfe4f0ca3e6ca6a1617d5a208
HyegyoJung <junghk0115@gmail.com>
2022-11-21 04:24:16 +0900

    Update 2533.cpp

--- 11월 15일 - 트리/선택/2533.cpp
+++ 11월 15일 - 트리/선택/2533.cpp
@@ -3,21 +3,27 @@
 
 using namespace std;
 
-vector<vector<int>> edges;
+vector<vector<int>> tree;
 vector<bool> visit;
-int dp[1000001][2]; //0 일 때 해당 정점이 얼리어답터 아닐 때, 1 일 떄 해당 정점이 얼리어답터 일 때
+vector<vector<int>> dp;
+
+/**
+ * dp에는 해당 정점을 루트로 가지는 서브 트리의 얼리어답터 최소 개수 저장
+ * dp[x][0] : x 정점이 얼리어답터가 아닐 때, 자식 노드는 반드시 얼리어답터여야 함.
+ * dp[x][1] : x 정점이 얼리어답터 일 때, 자식 노드가 얼리어답터이거나 아니어도 됨.
+*/
 
 void solution(int x){
 
     visit[x]=true;
     dp[x][1] = 1 ;
 
-    for(int i=0 ; i<edges[x].size(); i++){
-        int child = edges[x][i];
+    for(int i=0 ; i<tree[x].size(); i++){
+        int child = tree[x][i];
         if (!visit[child]){
             solution(child);
-            dp[x][0] += dp[child][1]; //현재 정점이 얼리어답터가 아닐 때,
-            dp[x][1] += min(dp[child][0], dp[child][1]); //현재 정점이 얼리어답터일 때
+            dp[x][0] += dp[child][1]; //현재 정점이 얼리어답터가 아닐 때
+            dp[x][1] += min(dp[child][0], dp[child][1]); //현재 정점이 얼리어답터 일 때
         }
     }
 
@@ -29,13 +35,15 @@ int main() {
     int n;
     cin >> n;
 
-    edges.resize(n+1);
-    visit.resize(n+1);
+    tree.assign(n+1, vector<int>(0));
+    visit.assign(n+1,0);
+    dp.assign(n+1, vector<int>(2,0));
     int u, v;
+
     for(int i=1; i<n; i++){
         cin >> u>>v;
-        edges[u].push_back(v);
-        edges[v].push_back(u);
+        tree[u].push_back(v);
+        tree[v].push_back(u);
     }
 
     //연산
