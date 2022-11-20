98a01319587d47e2499f176397c59eb369372342
밍 <79096808+0321minji@users.noreply.github.com>
2022-11-21 00:37:16 +0900

    Update 5639.cpp

--- 11월 15일 - 트리/선택/5639.cpp
+++ 11월 15일 - 트리/선택/5639.cpp
@@ -5,28 +5,28 @@ using namespace std;
 
 /*
 전위 순회로 입력된 노드 값을 후위 순위로 출력
-전위 순회 (자기 자신->left->right)가 재귀적으로 반복
-후위 순회 (left->right->자기 자신)이 재귀적으로 반복
+전위 순회 (root->left->right)가 재귀적으로 반복
+후위 순회 (left->right->root)이 재귀적으로 반복
  temp가 기준
  temp를 기준으로 작은 값이 있으면 해당 원소를 기준으로 왼쪽,오른쪽 나누어 다시 postOrder진행
 */
 
-void postOrder(int start, int end, vector<int> &tree) {
-	if (start > end) {
+void postOrder(int left, int right, vector<int> &tree) {
+	if (left > right) {
 		return;
 	}
-	int temp = start + 1;
+	int temp = left + 1;
 
-	for (int i = start + 1; i < end + 1; i++) {
-		if (tree[start] < tree[i]) {
+	for (int i = left + 1; i < right + 1; i++) {
+		if (tree[left] < tree[i]) {
 			temp = i;
 			break;
 		}
 	}
-	postOrder(start + 1, temp-1,tree);
-	postOrder(temp, end, tree);
+	postOrder(left + 1, temp-1,tree);
+	postOrder(temp, right, tree);
 	
-	cout << tree[start] << '\n';
+	cout << tree[left] << '\n';
 }
 
 int main() {
@@ -36,6 +36,5 @@ int main() {
 	while (cin >> input) {
 		tree.push_back(input);
 	}
-	//cout << tree.size()<<"\n";
 	postOrder(0, tree.size()-1,tree);
 }
