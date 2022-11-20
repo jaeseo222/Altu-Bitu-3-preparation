3e379284df583721d9c9b19f491c2e7591ae114d
김서연 <86418674+flowersayo@users.noreply.github.com>
2022-11-20 20:14:19 +0900

    Update 14675.cpp

--- 11월 15일 - 트리/선택/14675.cpp
+++ 11월 15일 - 트리/선택/14675.cpp
@@ -3,23 +3,37 @@
 
 using namespace std;
 
-/*
-단절점 : 연결된 간선이 2개이상
-단절선 : 모든 선이 단절선. ( cycle이 없기 때문)
-*/
+
+//단절점, 단절선 파악 함수
+string check(int t, int k, vector<vector<int>>& tree) {
+	if (t == 2) {
+		return "yes";
+	}
+		
+	if (tree[k].size() >= 2) {
+		return "yes";
+	}
+		
+	return "no";
+}
+
+/**
+ * 부모 노드를 알 수 없기에 트리를 양방향 그래프로 저장
+ * 단절점: 트리는 모든 정점이 연결되어 있고, 싸이클이 존재하지 않기 때문에 연결된 간선이 2개 이상이면 단절점
+ * 단절선: 트리는 모든 정점이 연결되어 있고, 싸이클이 존재하지 않기 때문에 모든 간선이 단절선
+ */
 
 int main() {
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	vector<vector<int>> tree;
-	
 
-	int n,v,u,test_case; 
+	int n, v, u, test_case;
 	cin >> n;
 
-	tree.assign(n + 1, vector<int>(0));
-	n--; 
+	vector<vector<int>> tree (n + 1, vector<int>(0));
+
+	n--;
 	while (n--) {
 		cin >> v >> u; // 무방향 그래프
 		tree[v].push_back(u);
@@ -31,14 +45,7 @@ int main() {
 	int t, k;
 	while (test_case--) {
 		cin >> t >> k;
-
-		if (t == 1) { //단절점체크
-			if (tree[k].size() >= 2)
-				cout << "yes\n";
-			else
-				cout << "no\n";
-		}
-		else if (t == 2) // 단절선체크
-			cout << "yes\n";
+		cout << check(t, k, tree) << '\n';
+						
 	}
 }
