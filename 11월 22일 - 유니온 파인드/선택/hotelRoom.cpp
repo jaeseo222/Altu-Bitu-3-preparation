cd6730705e7876ea7120b1178adef52a556fcff0
김서연 <86418674+flowersayo@users.noreply.github.com>
2022-11-25 01:31:02 +0900

    Update hotelRoom.cpp

--- 11월 22일 - 유니온 파인드/선택/hotelRoom.cpp
+++ 11월 22일 - 유니온 파인드/선택/hotelRoom.cpp
@@ -27,7 +27,7 @@ vector<ll> solution(ll k, vector<ll> room_number) {
 /**
  * [프로그래머스 - 호텔 방 배정]
  *
- * rooms 라는 map을 만들어 각 호텔당 투숙객 정보 저장
+ * parent 라는 map을 만들어 각 호텔방에서 다음 비어있는 방 정보 저장
  * 이때 key: 방 번호, value: 해당 방 다음에 있는 가장 가까운 빈 방 으로 설정하여
  * 원하는 방이 이미 배정되어있다면, 바로 다음 방을 탐색할 수 있도록 함.
  * 이때 그냥 map을 사용하면 시간초과, 탐색속도를 높이기 위해 Hash 방식으로 구현된 unordered_map 사용. 
